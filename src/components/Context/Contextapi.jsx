a4ae37c858cd8699b979cb71da1abfed1bfd2e56
vusale44 <vusalesalman1802@gmail.com>
2026-04-04 19:33:06 +0400

    mesaj

--- src/components/Context/Contextapi.jsx
+++ src/components/Context/Contextapi.jsx
@@ -13,7 +13,7 @@ useEffect(() => {
 
         const fetchProducts = async () => {
             try {
-                const response = await axios.get('http://localhost:3000/products');
+                const response = await axios.get('http://localhost:8000/products');
                 setProducts(response.data);
                 setLoading(false);
             } catch (error) {
